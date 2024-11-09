78a678f5aa965c28f708c8ecb6cb662c58c55933
xan <xxdr@duck.com>
2024-11-08 23:19:53 -0500

    refactor!(webview/bindings): remove damn windows support
    wtf is this code?

--- webview/bindings/build.rs
+++ webview/bindings/build.rs
@@ -1,78 +1,23 @@
-use cc::Build;
 use std::env;
-use std::fs;
-use std::path::PathBuf;
 
 fn main() {
-    let mut build = Build::new();
+    let mut build = cc::Build::new();
 
     let target = env::var("TARGET").unwrap();
 
     build
         .cpp(true)
         .include("ext/webview.h")
-        .flag_if_supported("/std:c++11")
         .flag_if_supported("-w");
 
-    // if env::var("DEBUG").is_err() {
-    //     build.define("NDEBUG", None);
-    // } else {
-    //     build.define("DEBUG", None);
-    // }
-
-    if target.contains("windows") {
-        // build.define("UNICODE", None); // doesn't work atm.
-        build
-            .file("ext/webview.cc")
-            .flag_if_supported("/std:c++17");
-
-        for &lib in &["windowsapp", "user32", "oleaut32", "ole32"] {
-            println!("cargo:rustc-link-lib={}", lib);
-        }
-
-        let webview2_arch = if target.contains("x86_64") {
-            "x64"
-        } else {
-            "x86"
-        };
-
-        // calculate full path to WebView2Loader.dll
-        let mut webview2_path_buf = PathBuf::from(env::current_dir().unwrap().to_str().unwrap());
-        webview2_path_buf
-            .push("webview/script/Microsoft.Web.WebView2.0.9.488/build/native");
-        webview2_path_buf.push(webview2_arch);
-        let webview2_dir = webview2_path_buf.as_path().to_str().unwrap();
-
-        let loader_asm_name = "WebView2Loader.dll";
-
-        println!("cargo:rustc-link-search={}", webview2_dir);
-        println!("cargo:rustc-link-lib={}", loader_asm_name);
-
-        // copy WebView2Loader.dll to `target/debug`
-        let mut src_asm_buf = PathBuf::from(webview2_dir);
-        src_asm_buf.push(loader_asm_name);
-
-        // we want to be able to calculate C:\crate\root\target\debug\
-        //           while we can get this ^^^^^^^^^^^^^   and  ^^^^^ from env::current_dir() and %PROFILE% respectively
-        // there's no way to get this (reliably)         ^^^^^^
-        // we can, however, use %OUT_DIR% (C:\crate\root\target\debug\build\webview_rust-xxxx\out\)
-        // and navigate backwards to here  ^^^^^^^^^^^^^^^^^^^^^^^^^^
-        let mut target_asm_buf = PathBuf::from(env::var("OUT_DIR").unwrap());
-        target_asm_buf.pop();
-        target_asm_buf.pop();
-        target_asm_buf.pop();
-        target_asm_buf.push(loader_asm_name);
-
-        fs::copy(src_asm_buf.as_path(), target_asm_buf.as_path()).unwrap();
-    } else if target.contains("apple") {
-        build.file("ext/webview.cc").flag("-std=c++11");
-
+    if target.contains("apple") {
+        build.file("ext/webview.cc");
         println!("cargo:rustc-link-lib=framework=Cocoa");
         println!("cargo:rustc-link-lib=framework=WebKit");
     } else if target.contains("linux") || target.contains("bsd") {
         let lib = pkg_config::Config::new()
             .atleast_version("2.8")
-            .probe("webkit2gtk-4.0")
+            .probe("webkit2gtk-4.0") // update this to latest, it's like at version 4.1 now
             .unwrap();
 
         for path in lib.include_paths {
@@ -85,7 +30,7 @@ fn main() {
 
         build.file("ext/webview.cc");
     } else {
-        panic!("Unsupported platform");
+        panic!("Unsupported platform, make a PR if you want support for your OS"); // I wonder if OSes like React, Haiku and Redox has webview...
     }
 
     println!("cargo:rerun-if-changed=ext/webview.h");
