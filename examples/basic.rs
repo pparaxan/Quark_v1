56f5a6fa889aeeb2716fd44a4db90b03a6c9eaaf
WilliamAnimate <nyameowmeow@duck.com>
2024-11-08 21:40:38 -0500

    revert: "style: rm commented out code"
    This reverts commit ce0156228e961790a6bc1eafcfe168081962c2c3.
    
    Reasoning:
    
    pushed changes that change the cargo version from 2024 to 2021, because
    i didn't actually wanna install a nightly compiler + nightly
    rust-analyzer

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,14 @@
 use crowsa::prelude::*;
 
 fn main() -> Result<(), CrowsaError> {
+    // let config = CrowsaConfig {
+    //     content_path: String::from("./examples/global_html"),
+    //     window_title: String::from("Hello World"),
+    //     width: 1024,
+    //     height: 768,
+    //     debug: true,
+    //     resizable: 1,
+    // };
     let config = CrowsaConfig::new()
         .content_path("./examples/global_html")
         .window_title("Hello World")
