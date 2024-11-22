8917bbb517d50cf3644f9e8f3f272467fcc274a2
xan <xxdr@duck.com>
2024-11-21 21:34:28 -0500

    refactor(hyaline): merge `hyaline-bindings`
    merge hyaline-bindings with hyaline, yeha that's it really

--- hyaline/src/lib.rs
+++ hyaline/src/lib.rs
@@ -1,5 +1,39 @@
+pub use builder::WebviewBuilder;
+pub use webview::{SizeHint, Webview, Window};
+use std::os::raw::{c_char, c_int, c_void};
+pub type DispatchFn = extern "C" fn(webview: webview_t, arg: *mut c_void);
+pub type BindFn = extern "C" fn(seq: *const c_char, req: *const c_char, arg: *mut c_void);
+
 mod builder;
 mod webview;
 
-pub use builder::WebviewBuilder;
-pub use webview::{SizeHint, Webview, Window};
+#[allow(non_camel_case_types)]
+pub type webview_t = *mut c_void;
+
+unsafe extern "C" {
+    pub unsafe fn webview_create(debug: c_int, window: *mut c_void) -> webview_t;
+
+    pub unsafe fn webview_destroy(w: webview_t);
+
+    pub unsafe fn webview_run(w: webview_t);
+
+    pub unsafe fn webview_terminate(w: webview_t);
+
+    pub unsafe fn webview_dispatch(w: webview_t, fn_: Option<DispatchFn>, arg: *mut c_void);
+
+    pub unsafe fn webview_get_window(w: webview_t) -> *mut c_void;
+
+    pub unsafe fn webview_set_title(w: webview_t, title: *const c_char);
+
+    pub unsafe fn webview_set_size(w: webview_t, width: c_int, height: c_int, hints: c_int);
+
+    pub unsafe fn webview_navigate(w: webview_t, url: *const c_char);
+
+    pub unsafe fn webview_init(w: webview_t, js: *const c_char);
+
+    pub unsafe fn webview_eval(w: webview_t, js: *const c_char);
+
+    pub unsafe fn webview_bind(w: webview_t, name: *const c_char, fn_: Option<BindFn>, arg: *mut c_void);
+
+    pub unsafe fn webview_return(w: webview_t, seq: *const c_char, status: c_int, result: *const c_char);
+}
--- hyaline/src/webview.rs
+++ hyaline/src/webview.rs
@@ -4,7 +4,7 @@ use std::os::raw::*;
 use std::ptr::null_mut;
 use std::sync::Arc;
 
-use hyaline_bindings as sys;
+use crate as bindings;
 
 pub enum Window {}
 
@@ -29,7 +29,7 @@ impl Default for SizeHint {
 
 #[derive(Clone)]
 pub struct Webview {
-    inner: Arc<sys::webview_t>,
+    inner: Arc<bindings::webview_t>,
     url: String,
 }
 
@@ -40,8 +40,8 @@ impl Drop for Webview {
     fn drop(&mut self) {
         if Arc::strong_count(&self.inner) == 0 {
             unsafe {
-                sys::webview_terminate(*self.inner);
-                sys::webview_destroy(*self.inner);
+                bindings::webview_terminate(*self.inner);
+                bindings::webview_destroy(*self.inner);
             }
         }
     }
@@ -52,13 +52,13 @@ impl Webview {
         if let Some(w) = window {
             Webview {
                 inner: Arc::new(unsafe {
-                    sys::webview_create(debug as c_int, w as *mut Window as *mut _)
+                    bindings::webview_create(debug as c_int, w as *mut Window as *mut _)
                 }),
                 url: "".to_string(),
             }
         } else {
             Webview {
-                inner: Arc::new(unsafe { sys::webview_create(debug as c_int, null_mut()) }),
+                inner: Arc::new(unsafe { bindings::webview_create(debug as c_int, null_mut()) }),
                 url: "".to_string(),
             }
         }
@@ -66,26 +66,26 @@ impl Webview {
 
     pub fn run(&mut self) {
         let c_url = CString::new(self.url.as_bytes()).expect("No null bytes in parameter url");
-        unsafe { sys::webview_navigate(*self.inner, c_url.as_ptr()) }
-        unsafe { sys::webview_run(*self.inner) }
+        unsafe { bindings::webview_navigate(*self.inner, c_url.as_ptr()) }
+        unsafe { bindings::webview_run(*self.inner) }
     }
 
     pub fn terminate(&mut self) {
-        unsafe { sys::webview_terminate(*self.inner) }
+        unsafe { bindings::webview_terminate(*self.inner) }
     }
 
     // TODO Window instance
     pub fn set_title(&mut self, title: &str) {
         let c_title = CString::new(title).expect("No null bytes in parameter title");
-        unsafe { sys::webview_set_title(*self.inner, c_title.as_ptr()) }
+        unsafe { bindings::webview_set_title(*self.inner, c_title.as_ptr()) }
     }
 
     pub fn set_size(&mut self, width: i32, height: i32, hints: SizeHint) {
-        unsafe { sys::webview_set_size(*self.inner, width, height, hints as i32) }
+        unsafe { bindings::webview_set_size(*self.inner, width, height, hints as i32) }
     }
 
     pub fn get_window(&self) -> *mut Window {
-        unsafe { sys::webview_get_window(*self.inner) as *mut Window }
+        unsafe { bindings::webview_get_window(*self.inner) as *mut Window }
     }
 
     pub fn navigate(&mut self, url: &str) {
@@ -94,12 +94,12 @@ impl Webview {
 
     pub fn init(&mut self, js: &str) {
         let c_js = CString::new(js).expect("No null bytes in parameter js");
-        unsafe { sys::webview_init(*self.inner, c_js.as_ptr()) }
+        unsafe { bindings::webview_init(*self.inner, c_js.as_ptr()) }
     }
 
     pub fn eval(&mut self, js: &str) {
         let c_js = CString::new(js).expect("No null bytes in parameter js");
-        unsafe { sys::webview_eval(*self.inner, c_js.as_ptr()) }
+        unsafe { bindings::webview_eval(*self.inner, c_js.as_ptr()) }
     }
 
     pub fn dispatch<F>(&mut self, f: F)
@@ -107,7 +107,7 @@ impl Webview {
         F: FnOnce(&mut Webview) + Send + 'static,
     {
         let closure = Box::into_raw(Box::new(f));
-        extern "C" fn callback<F>(webview: sys::webview_t, arg: *mut c_void)
+        extern "C" fn callback<F>(webview: bindings::webview_t, arg: *mut c_void)
         where
             F: FnOnce(&mut Webview) + Send + 'static,
         {
@@ -118,7 +118,7 @@ impl Webview {
             let closure: Box<F> = unsafe { Box::from_raw(arg as *mut F) };
             (*closure)(&mut webview);
         }
-        unsafe { sys::webview_dispatch(*self.inner, Some(callback::<F>), closure as *mut _) }
+        unsafe { bindings::webview_dispatch(*self.inner, Some(callback::<F>), closure as *mut _) }
     }
 
     pub fn bind<F>(&mut self, name: &str, f: F)
@@ -146,7 +146,7 @@ impl Webview {
             mem::forget(f);
         }
         unsafe {
-            sys::webview_bind(
+            bindings::webview_bind(
                 *self.inner,
                 c_name.as_ptr(),
                 Some(callback::<F>),
@@ -158,6 +158,6 @@ impl Webview {
     pub fn r#return(&self, seq: &str, status: c_int, result: &str) {
         let c_seq = CString::new(seq).expect("No null bytes in parameter seq");
         let c_result = CString::new(result).expect("No null bytes in parameter result");
-        unsafe { sys::webview_return(*self.inner, c_seq.as_ptr(), status, c_result.as_ptr()) }
+        unsafe { bindings::webview_return(*self.inner, c_seq.as_ptr(), status, c_result.as_ptr()) }
     }
 }
