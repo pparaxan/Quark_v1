1d0682b0555b1578ee0401482264aeec8adc3faf
xan <xxdr@duck.com>
2024-11-23 10:19:26 -0500

    refactor(hyaline/build.rs): bump to `webkit2gtk-4.1`, `rerun-if-changed` now points to `webview.cc`

--- hyaline/build.rs
+++ hyaline/build.rs
@@ -2,29 +2,30 @@ use std::env;
 
 fn main() {
     let mut build = cc::Build::new();
-
     let target = env::var("TARGET").unwrap();
 
+    build.cpp(true); // universal
+
     if target.contains("apple") {
-        build.file("webview.cc").cpp(true);
+        build.file("webview.cc");
         println!("cargo:rustc-link-lib=framework=Cocoa");
         println!("cargo:rustc-link-lib=framework=WebKit");
     } else if target.contains("linux") || target.contains("bsd") {
         let lib = pkg_config::Config::new()
             .atleast_version("2.8")
-            .probe("webkit2gtk-4.0") // update this to latest, it's like at version 4.1 now
+            .probe("webkit2gtk-4.1")
             .unwrap();
 
         for path in lib.include_paths {
             build.include(path);
         }
 
-        build.file("webview.cc").cpp(true);
+        build.file("webview.cc");
     } else {
         panic!("Unsupported target, make a issue if you want support for this operating system.");
     }
 
-    println!("cargo:rerun-if-changed=ext/webview.cc");
+    println!("cargo:rerun-if-changed=webview.cc");
 
     build.compile("webview");
 }
