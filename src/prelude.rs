cdfc150ea4d4588393039f6aeebff733a06d4330
xan <xxdr@duck.com>
2024-11-22 20:21:18 -0500

    chore: i dont know what i should name this commit
    dont mind

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 //! # The Quark prelude
 //!
-//! This prelude contains the bare essentals to get a Quark project up and going. Use it with `use Quark::prelude::*;` at the top of your `main.rs` file!
+//! This prelude contains the bare essentals to get a Quark project up and going. Use it with `use quark::prelude::*;` at the top of your `main.rs` file!
 
 pub use crate::{Quark, config::QuarkConfig, error::QuarkError};
 pub use hyaline::SizeHint;
