d5f7bcf7ef7de0b8980112d3100450487f4b2baf
xan <xxdr@duck.com>
2024-11-25 11:21:15 -0500

    refactor!(examples): prep for the major update; move frontend folder to root and renamed to `src_quark`

--- examples/helloworld.rs
+++ examples/helloworld.rs
@@ -2,7 +2,6 @@ use quark::prelude::*;
 
 fn main() -> Result<(), QuarkError> {
     let config = QuarkConfig::new()
-        .frontend("examples/frontend/helloworld")
         .title("Hello World")
         .resizable(SizeHint::FIXED);
 
