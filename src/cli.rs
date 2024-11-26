52ee7630b3c4aaab40f4c944da3c76d4d39d9f1e
xan <xxdr@duck.com>
2024-11-26 16:16:53 -0500

    feat: cli
    i'll link up the code together later, by later i mean the next 5 mins

---
+++ src/cli.rs
@@ -0,0 +1,38 @@
+#[derive(Debug)]
+pub struct Args {
+    pub live: bool,
+    pub bundle: bool,
+}
+
+pub fn parse_args() -> Args {
+    // https://github.com/WilliamAnimate/catgirls_anytime/blob/849c973e8e355cb6ae0695e287764299c6c2543d/src/lib.rs#L18-L76
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut parsed_args = Args {
+        live: false,
+        bundle: false,
+    };
+
+    for args in &args[1..] {
+        match args.as_str() {
+            "--help" => {
+                println!("Usage: cargo run -- [OPTION]");
+                println!("--live          Start a live server with hot reload support");
+                println!("--bundle        Package your Quark application for your target [WIP]");
+                println!("--help          Display this help message and exit");
+                std::process::exit(0);
+            }
+            "--live" => {
+                parsed_args.live = true;
+            }
+            "--bundle" => {
+                println!("Error: This argument isn't complete!");
+                std::process::exit(1);
+            }
+            other => eprintln!(
+                "'{other}' is an unknown argument silly, do '--help' to list the commands."
+            ),
+        }
+    }
+    parsed_args
+}
